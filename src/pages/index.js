51e60d0416d1120c8bfc430f811c4d9cd8eb0186
el3ctricSol <aubrey.ky.young@gmail.com>
2018-06-12 02:03:29 -0700

    added video; finish communicate page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 import styled from "styled-components";
 
-import Hero from '../images/hero-image.png'
+// import Hero from '../images/hero-image.png';
+import VCR from '../images/vcr-intro.mp4';
 
 const IndexContainer = styled.div`
   ${'' /* padding: '5rem 1.0875rem 1.45rem'; */}
@@ -12,15 +13,15 @@ const IndexContainer = styled.div`
   img {
     ${'' /* width: 100%; */}
   }
- 
-
- 
 `;
 
 const IndexPage = () => {
   return (
   <IndexContainer>
-    <img src={Hero} alt="Hero movie"/>
+    {/* <img src={Hero} alt="Hero movie"/> */}
+    <video autoplay width="100%" >
+      <source src={VCR} type="video/mp4"/>
+    </video>
   </IndexContainer>
   
 )
